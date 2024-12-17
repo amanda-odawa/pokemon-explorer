bf3b1965a91d2afb62b8e2aa7b9ad9e278791fe7
AmandaOdawa <amandaodawa1@gmail.com>
2024-12-17 14:20:31 +0300

    fetch data

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const pokemonList = document.getElementById('pokemon-list');
+    const pokemonModal = document.getElementById('pokemon-modal');
+    const closeModal = document.getElementById('close-modal');
+    const toggleThemeButton = document.getElementById('toggle-theme');
+    const viewFavoritesButton = document.getElementById('view-favorites');
+
+    //Fetch Pokémon data
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=200')
+        .then(response => response.json())
+        .then(data => {
+            const randomPokemons = getRandomPokemons(data.results, 15);
+
+            randomPokemons.forEach(pokemon => {
+                fetch(pokemon.url)
+                    .then(response => response.json())
+                    .then(pokemonDetails => {
+                        const card = document.createElement('div');
+                        card.classList.add('pokemon-crad');
+                        card.innerHTML = 
+                        `
+                        <img src="${pokemonDetails.sprites.front_default}" alt="${pokemonDetails.name}">
+                        <h3>${pokemonDetails.name}</h3>
+                        <button class="star-btn" data-id="${pokemonDetails.id}">\u2B50</button>
+                        `;
+                        card.addEventListener('click', () => openModal(pokemonDetails));
+                        pokemon.appendChild(card);
+
+                        
+                    })
+            });
+        })
+})
